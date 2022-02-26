6db9493f823e2d6c39f7102ba25ee67dad3d5e1a
Saeid Amiri <saeid.amiri1@gmail.com>
2022-02-25 22:58:33 -0500

    Update general.py

--- python/general.py
+++ python/general.py
@@ -1,5 +1,12 @@
-def hello():
-  print("Hello world")
-  
-def hello2():
-  print("Hello world")
+import time 
+def timeit(fn): 
+    # *args and **kwargs are to support positional and named arguments of fn
+    def get_time(*args, **kwargs): 
+        start = time.time() 
+        output = fn(*args, **kwargs)
+        print(f"Time taken in {fn.__name__}: {time.time() - start:.7f}")
+        return output  # make sure that the decorator returns the output of fn
+    return get_time
+
+###
+dollarizer = lambda x: float(x[1:-1])
