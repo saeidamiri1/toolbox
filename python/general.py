b8246e5eaaeea21c361e273c3680b243ebd8cd40
Saeid Amiri <saeid.amiri1@gmail.com>
2023-04-06 11:35:17 -0400

    Update general.py

--- python/general.py
+++ python/general.py
@@ -7,7 +7,11 @@ def timeit(fn):
         print(f"Time taken in {fn.__name__}: {time.time() - start:.7f}")
         return output  # make sure that the decorator returns the output of fn
     return get_time
+@timeit
+def pow(a, b):
+    return(a**b)
 
+pow(10, 10)
 ###
 dollarizer = lambda x: float(x[1:-1])
 
@@ -56,3 +60,50 @@ with zipfile.ZipFile(file_name, 'r') as zip_ref:
 import gzip
 open_function = lambda f: gzip.open(f,"rt") if f[-3:] == ".gz" else open(f)
 
+
+
+#
+
+# Built-in modules #
+import re, hashlib, unicodedata
+
+# One liners #
+flatter = lambda x: [item for sublist in x for item in sublist]
+
+################################################################################
+def sanitize_text(text):
+    """Make a safe representation of a string.
+    Note: the `\s` special character matches any whitespace character.
+    This is equivalent to the set [\t\n\r\f\v] as well as ` ` (whitespace)."""
+    # First replace characters that have specific effects with their repr #
+    text = re.sub("(\s)", lambda m: repr(m.group(0)).strip("'"), text)
+    # Make it a unicode string (the try supports python 2 and 3) #
+    try: text = text.decode('utf-8')
+    except AttributeError: pass
+    # Normalize it “
+    text = unicodedata.normalize('NFC', text)
+    return text
+
+###############################################################################
+def natural_sort(item):
+    """
+    Sort strings that contain numbers correctly. Works in Python 2 and 3.
+
+    >>> l = ['v1.3.12', 'v1.3.3', 'v1.2.5', 'v1.2.15', 'v1.2.3', 'v1.2.1']
+    >>> l.sort(key=natural_sort)
+    >>> l.__repr__()
+    "['v1.2.1', 'v1.2.3', 'v1.2.5', 'v1.2.15', 'v1.3.3', 'v1.3.12']"
+    """
+    dre = re.compile(r'(\d+)')
+    return [int(s) if s.isdigit() else s.lower() for s in re.split(dre, item)]
+
+################################################################################
+def md5sum(file_path, blocksize=65536):
+    """Compute the md5 of a file. Pretty fast."""
+    result = hashlib.md5()
+    with open(file_path, "rb") as f:
+        chunk = f.read(blocksize)
+        while chunk:
+            result.update(chunk)
+            chunk = f.read(blocksize)
+    return result.hexdigest()
