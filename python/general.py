14b99390cb1d92311e53ab0894ef20bbb8fe8178
Saeid Amiri <saeid.amiri1@gmail.com>
2023-04-15 15:52:25 -0400

    Update general.py

--- python/general.py
+++ python/general.py
@@ -132,3 +132,27 @@ def run_sub_(cmd):
     return ([x.strip().decode("utf-8") for x in out])
 
 run_sub_('ls')
+
+########### chck for duplication 
+import os
+from subprocess import (Popen,PIPE)
+
+def find_duplicates(path):
+    checksums = {}
+    for root, _, files in os.walk(path):
+        for name in files:
+            fullpath = os.path.join(root,name)
+            print(f"Processing file: {fullpath}")
+            with Popen(["md5sum", fullpath],stdout=PIPE) as proc:
+                checksum, path = proc.stdout.read().split()
+                if checksum in checksums:
+                    print(f"Found duplicate files: {fullpath} {checksums[checksum]}")
+                checksums[checksum] = fullpath
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) == 2:
+        npath = sys.argv[1]
+        find_duplicates(npath)
+        sys.exit(0)
+    print("Pass in path to find duplicates")
