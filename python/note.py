663dccf03b04afdd4ccbfb28002359db6f1f7b7d
Saeid Amiri <saeid.amiri1@gmail.com>
2023-04-17 11:44:13 -0400

    Create note.py

---
+++ python/note.py
@@ -0,0 +1,15 @@
+def uppload_via_github(url): 
+    import sys
+    import os 
+    import requests
+    r = requests.get(url)
+    file_name=os.path.basename(url).split('/')[-1]
+    with open(file_name, 'w') as f:
+        f.write(r.text)
+    exec(f"import {file_name.split('.')[-2]}")
+
+url='https://raw.githubusercontent.com/saeidamiri1/toolbox/main/python/utils.py'
+uppload_via_github(url)
+
+ 
+
