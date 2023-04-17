b8b9009d03e2ef08f517bb3d51db7b8f098b6131
Saeid Amiri <saeid.amiri1@gmail.com>
2023-04-17 11:50:03 -0400

    Update utils.py

--- python/utils.py
+++ python/utils.py
@@ -5,3 +5,14 @@ def file_parts(path0):
     file_base=file_name.split('.')[-2]
     return(file_path,file_name,file_base,file_ext)
     
+def reload_frompath(module0,path0): 
+    import sys
+    from importlib import reload
+    sys.path.append(path0)
+    exec(f"import {source}")
+    exec(f"reload {source}")
+
+# path0='/home/samamiri/runs/samamiri/general_files/VeParser/VeParser/'
+# module0='source'
+# reload_frompath(module0,path0)
+
